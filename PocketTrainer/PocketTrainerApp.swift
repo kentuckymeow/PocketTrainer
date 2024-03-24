277ae59bab6b8c035700137669edbf0bb0e359f2
Arseni Khatsuk <khatsuk007@gmail.com>
2024-03-24 23:18:09 +0300

    Initial Commit

---
+++ PocketTrainer/PocketTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketTrainerApp.swift
+//  PocketTrainer
+//
+//  Created by Arseni Khatsuk on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PocketTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
